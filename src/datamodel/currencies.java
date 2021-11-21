fd8ec13baccde780d23afddbb9d2c39934a312ea
EricTheBarbarian <emckinney@unomaha.edu>
2021-11-20 23:10:12 -0600

    updated .java files for SQL tables

---
+++ src/datamodel/currencies.java
@@ -0,0 +1,60 @@
+package datamodel;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @since J2SE-1.8
+ CREATE TABLE currencies (
+  currencyID INT NOT NULL AUTO_INCREMENT,    
+  currencyName VARCHAR(30) NOT NULL,   
+  PRIMARY KEY (currencyID));
+ */
+@Entity
+@Table(name = "currencies")
+public class currencies {
+
+   @currencyID  // primary key
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Column(name = "currencyID") // specify the column name. Without it, it will use method name
+   private Integer currencyID;
+
+   @Column(name = "currencyName")
+   private String userName;
+ 
+   public User() {
+	  super();
+   }
+
+   public currencies(Integer currencyID, String currencyName) {
+      this.currencyID = currencyID;
+      this.currencyName = currencyName;
+   }
+
+
+   public Integer getCurrencyID() {
+      return currencyID;
+   }
+
+   public void setCurrencyID(Integer currencyID) {
+      this.currencyID = currencyID;
+   }
+
+   public String getcurrencyName() {
+      return currencyName;
+   }
+
+   public void setCurrencyName(String currencyName) {
+      this.currencyName = currencyName;
+   }
+  
+
+   @Override
+   public String toString() {
+      return "Currencies: " + this.currencyID + ", " + this.currencyName;
+   }
+}
---
+++ src/datamodel/follows.java
@@ -0,0 +1,84 @@
+package datamodel;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @since J2SE-1.8
+ CREATE TABLE follows (
+  followID INT NOT NULL AUTO_INCREMENT,    
+  currencyID INT NOT NULL,   
+  userID INT NOT NULL,
+  PRIMARY KEY (followID) FOREIGN KEY (userID) 
+        REFERENCES users(userID),
+    FOREIGN KEY (currencyID) 
+        REFERENCES currencies(currencyID));
+ */
+@Entity
+@Table(name = "follows")
+public class follows {
+
+   @followID  // primary key
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Column(name = "followID") // specify the column name. Without it, it will use method name
+   private Integer followID;
+
+   @Column(name = "currencyID")
+   private int currencyID;
+   
+   @Column(name = "userID")
+   private int userID;
+
+
+
+
+
+   public User() {
+	  super();
+   }
+
+   public User(Integer followID, Integer currencyID, Integer userID) {
+      this.followID = followID;
+      this.currencyID = currencyID;
+      this.userID = userID;
+   }
+
+   public User(Integer currencyID, Integer userID) {
+      this.currencyID = currencyID;
+      this.userID = userID;
+   }
+
+   public Integer getfollowID() {
+      return followID;
+   }
+
+   public void setFollowID(Integer followID) {
+      this.followID = followID;
+   }
+
+   public Integer getCurrencyID() {
+      return currencyID;
+   }
+
+   public void setCurrencyID(Integer currencyID) {
+      this.currencyID = currencyID;
+   }
+   
+   public Integer getUserID() {
+      return userID;
+   }
+
+   public void setUserID(Integer userID) {
+      this.userID = userID;
+   }
+
+
+   @Override
+   public String toString() {
+      return "Follows: " + this.followID + ", " + this.currencyID + ", " + this.userID;
+   }
+}
